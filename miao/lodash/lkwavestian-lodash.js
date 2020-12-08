77652ca788da69abf9aec0f1183c7d6329bb7b89
Tian-alt <775925189@qq.com>
2020-12-08 18:57:03 +0800

    dfdfdf

--- miao/lodash/lkwavestian-lodash.js
+++ miao/lodash/lkwavestian-lodash.js
@@ -17,15 +17,16 @@ var lkwavestian = function () {
   }
 
   function isFalse(val) {
-    if(val === false || val === null || val === 0 || val === "" || val === undefined || val !== val)
+    if (val === false || val === null || val === 0 || val === "" || val === undefined || val !== val)
       return true
     else
       return false
   }
+
   function compact(ary) {
     var res = new Array
     for (let i = 0; i < ary.length; ++i) {
-      if(!isFalse(ary[i]))
+      if (!isFalse(ary[i]))
         res.push(ary[i])
     }
     return res
@@ -33,20 +34,21 @@ var lkwavestian = function () {
 
   function drop(ary, num = 1) {
     var len = ary.length
-    if(num >= len)
-      return[]
+    if (num >= len)
+      return []
     var res = []
-    for(let i = num; i < len; ++i) {
+    for (let i = num; i < len; ++i) {
       res.push(ary[i])
     }
     return res
   }
+
   function dropRight(ary, num = 1) {
     var len = ary.length
-    if(num >= len)
-      return[]
+    if (num >= len)
+      return []
     var res = []
-    for(let i = len - num - 1; i >= 0; --i) {
+    for (let i = len - num - 1; i >= 0; --i) {
       res.unshift(ary[i])
     }
     return res
@@ -54,8 +56,8 @@ var lkwavestian = function () {
 
   function join(ary, sep) {
     var res = ''
-    for(let i = 0; i < ary.length; ++i) {
-      if(i === ary.length - 1)
+    for (let i = 0; i < ary.length; ++i) {
+      if (i === ary.length - 1)
         res += ary[i]
       else
         res = res + ary[i] + sep
@@ -68,36 +70,93 @@ var lkwavestian = function () {
   }
 
   function lastIndexOf(ary, val, fromIdex = ary.length - 1) {
-    for(let i = fromIdex; i >=0; --i) {
-      if(ary[i] === val)
+    for (let i = fromIdex; i >= 0; --i) {
+      if (ary[i] === val)
         return i
     }
     return -1
   }
 
   function fill(ary, val, start = 0, end = ary.length) {
-    for(let i = start; i < end; ++i) {
+    for (let i = start; i < end; ++i) {
       ary[i] = val
     }
     return ary
   }
 
   function findIndex(ary, predicate, fromIndex = 0) {
-    for(let i = fromIndex; i < ary.length; ++i) {
-      if(predicate(ary[i]))
+    for (let i = fromIndex; i < ary.length; ++i) {
+      if (predicate(ary[i]))
         return i
     }
     return -1
   }
 
   function findLastIndex(ary, predicate, fromIndex = ary.length - 1) {
-    for(let i = fromIndex; i >= 0; --i) {
-      if(predicate(ary[i]))
+    for (let i = fromIndex; i >= 0; --i) {
+      if (predicate(ary[i]))
         return i
     }
     return -1
   }
+
+  /* function flatten(ary) {
+    let res = new Array
+
+    function
+    for (let i = 0; i < ary.length; ++i) {
+      if (ary[i] instanceof Array) {
+        for (let j = 0; j < )
+      }
+    }
+  } */
+
+  function flattenDeep(ary) {
+    var res = new Array
+    var recursion = ary => {
+      for (let i = 0; i < ary.length; ++i) {
+        if (!(ary[i] instanceof Array))
+          res.push(ary[i])
+        else
+          recursion(ary[i])
+      }
+      return res
+    }
+    return recursion(ary)
+  }
+
+  function fromPairs(ary) {
+    var map = new Map()
+    for(let i = 0; i < ary.length; ++i) {
+      map.set(ary[i][0], ary[i][1])
+    }
+    return map
+  }
+
+  function head(ary) {
+      return ary[0]
+  }
+
+  function indexOf(ary, val, fromIndex = 0) {
+    for(let i = fromIndex; i < ary.length; ++i) {
+      if(ary[i] === val)
+        return i
+    }
+    return -1
+  }
+
+  function initial(ary) {
+    let res = []
+    for(let i = 0; i < ary.length - 1; ++i) {
+      res.push(ary[i])
+    }
+  }
   return {
+    initial,
+    indexOf,
+    head,
+    fromPairs,
+    flattenDeep,
     findLastIndex,
     findIndex,
     fill,
