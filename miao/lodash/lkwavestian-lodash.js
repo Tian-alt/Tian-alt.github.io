822a9b76222c919a254ebcf56c222b1df8b4a06c
Tian-alt <775925189@qq.com>
2020-12-08 20:49:02 +0800

    dfdffdff

--- miao/lodash/lkwavestian-lodash.js
+++ miao/lodash/lkwavestian-lodash.js
@@ -126,20 +126,20 @@ var lkwavestian = function () {
   }
 
   function fromPairs(ary) {
-    var map = new Map()
-    for(let i = 0; i < ary.length; ++i) {
-      map.set(ary[i][0], ary[i][1])
+    var map = {}
+    for (let i = 0; i < ary.length; ++i) {
+      map[ary[i][0]] = ary[i][1]
     }
     return map
   }
 
   function head(ary) {
-      return ary[0]
+    return ary[0]
   }
 
   function indexOf(ary, val, fromIndex = 0) {
-    for(let i = fromIndex; i < ary.length; ++i) {
-      if(ary[i] === val)
+    for (let i = fromIndex; i < ary.length; ++i) {
+      if (ary[i] === val)
         return i
     }
     return -1
@@ -147,11 +147,142 @@ var lkwavestian = function () {
 
   function initial(ary) {
     let res = []
-    for(let i = 0; i < ary.length - 1; ++i) {
+    for (let i = 0; i < ary.length - 1; ++i) {
       res.push(ary[i])
     }
+    return initial
   }
+
+  function reverse(ary) {
+    var left = 0
+    var right = ary.length - 1
+    while (left < right) {
+      let temp = ary[left]
+      ary[left] = ary[right]
+      ary[right] = temp
+        ++left
+        --right
+    }
+    return ary
+  }
+
+  function sortedIndex(ary, val) {
+    var res = ary.length
+    var left = 0
+    var right = ary.length - 1
+    while (left <= right) {
+      let mid = (left + right) >> 1
+      if (val <= ary[mid]) {
+        res = mid
+        right = mid - 1
+      } else
+        left = mid + 1
+    }
+    return res
+  }
+
+  function every(ary, predicate) {
+    for (let i = 0; i < ary.length; ++i) {
+      if (!predicate(ary[i], i, ary))
+        return false
+    }
+    return true
+  }
+
+  function filter(ary, predicate) {
+    var res = []
+    for (let i = 0; i < ary.length; ++i) {
+      if (predicate(ary[i], i, ary))
+        res.push(ary[i])
+    }
+    return res
+  }
+
+  function find(ary, predicate) {
+    var res = undefined
+    for (let i = 0; i < ary.length; ++i) {
+      if (predicate(ary[i], i, ary)) {
+        res = ary[i]
+        break
+      }
+    }
+    return res
+  }
+
+  function toArray(val) {
+    var res = []
+    var i = 0
+    for (var key in val) {
+      res.push(val[key])
+    }
+    return res
+  }
+
+  function max(ary) {
+    let max = ary[0]
+    for (let i = 1; i < ary.length; ++i) {
+      if (max < ary[i])
+        max = ary[i]
+    }
+    return max
+  }
+
+  function maxBy(ary, iterate) {
+    var max = 0
+    for (let i = 1; i < ary.length; ++i) {
+      if (iterate(ary[max]) < iterate(ary[i]))
+        max = i
+    }
+    return ary[max]
+  }
+
+  function min(ary) {
+    let min = ary[0]
+    for (let i = 1; i < ary.length; ++i) {
+      if (min > ary[i])
+        min = ary[i]
+    }
+    return min
+  }
+
+  function minBy(ary, iterate) {
+    var min = 0
+    for (let i = 1; i < ary.length; ++i) {
+      if (iterate(ary[min]) > iterate(ary[i]))
+        min = i
+    }
+    return ary[min]
+  }
+
+  function sum(ary) {
+    var sum = 0
+    for (let i = 0; i < ary.length; ++i) {
+      sum += ary[i]
+    }
+    return sum
+  }
+
+  function sumBy(ary, iterate) {
+    var sum = 0
+    for (let i = 0; i < ary.length; ++i) {
+      sum += iterate(ary[i])
+    }
+    return sum
+  }
+
   return {
+    sumBy,
+    sum,
+    minBy,
+    min,
+    maxBy,
+    max,
+    toArray,
+    find,
+    filter,
+    every,
+    sortedIndex,
+    reverse,
     initial,
     indexOf,
     head,
