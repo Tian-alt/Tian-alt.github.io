d6e385811db467dd9e636d35478226fd0a432aaa
Tian-alt <775925189@qq.com>
2020-12-08 17:12:27 +0800

    fgfgfgf

--- miao/lodash/lkwavestian-lodash.js
+++ miao/lodash/lkwavestian-lodash.js
@@ -41,7 +41,70 @@ var lkwavestian = function () {
     }
     return res
   }
+  function dropRight(ary, num = 1) {
+    var len = ary.length
+    if(num >= len)
+      return[]
+    var res = []
+    for(let i = len - num - 1; i >= 0; --i) {
+      res.unshift(ary[i])
+    }
+    return res
+  }
+
+  function join(ary, sep) {
+    var res = ''
+    for(let i = 0; i < ary.length; ++i) {
+      if(i === ary.length - 1)
+        res += ary[i]
+      else
+        res = res + ary[i] + sep
+    }
+    return res
+  }
+
+  function last(ary) {
+    return ary[ary.length - 1]
+  }
+
+  function lastIndexOf(ary, val, fromIdex = ary.length - 1) {
+    for(let i = fromIdex; i >=0; --i) {
+      if(ary[i] === val)
+        return i
+    }
+    return -1
+  }
+
+  function fill(ary, val, start = 0, end = ary.length) {
+    for(let i = start; i < end; ++i) {
+      ary[i] = val
+    }
+    return ary
+  }
+
+  function findIndex(ary, predicate, fromIndex = 0) {
+    for(let i = fromIndex; i < ary.length; ++i) {
+      if(predicate(ary[i]))
+        return i
+    }
+    return -1
+  }
+
+  function findLastIndex(ary, predicate, fromIndex = ary.length - 1) {
+    for(let i = fromIndex; i >= 0; --i) {
+      if(predicate(ary[i]))
+        return i
+    }
+    return -1
+  }
   return {
+    findLastIndex,
+    findIndex,
+    fill,
+    lastIndexOf,
+    last,
+    join,
+    dropRight,
     drop,
     compact,
     chunk,
