d830505daa57d6b3296fe7f5649c4e034fb49418
Tian-alt <775925189@qq.com>
2020-12-08 15:56:09 +0800

    rfgfg

--- miao/lodash/lkwavestian-lodash.js
+++ miao/lodash/lkwavestian-lodash.js
@@ -1,8 +1,22 @@
 var lkwavestian = function() {
-  function compact(ary) {
-    
+
+  function chunk(ary, size) {
+    let len = ary.length
+    let num = Math.ceil(len / size)
+    let result = new Array(num)
+    let k = 0
+    for (let i = 0; i < num; ++i) {
+      result[i] = new Array
+      for (let j = 0; j < size; ++j) {
+        result[i].push(ary[k++])
+        if(k == len)
+          return result
+      }
+    }
+    return result
   }
+  
   return {
-    compact,
+    chunk,
   }
 }()
