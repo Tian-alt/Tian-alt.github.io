82ebd5491a3733e44c76d2494ce594d570ef50e7
Tian-alt <775925189@qq.com>
2020-12-08 16:29:10 +0800

    ffgfg

--- miao/lodash/lkwavestian-lodash.js
+++ miao/lodash/lkwavestian-lodash.js
@@ -1,4 +1,4 @@
-var lkwavestian = function() {
+var lkwavestian = function () {
 
   function chunk(ary, size) {
     var len = ary.length
@@ -9,15 +9,41 @@ var lkwavestian = function() {
       result[i] = new Array
       for (var j = 0; j < size; ++j) {
         result[i].push(ary[k++])
-        if(k == len)
+        if (k == len)
           return result
       }
     }
     return result
   }
 
-  function compact()
+  function isFalse(val) {
+    if(val === false || val === null || val === 0 || val === "" || val === undefined || val !== val)
+      return true
+    else
+      return false
+  }
+  function compact(ary) {
+    var res = new Array
+    for (let i = 0; i < ary.length; ++i) {
+      if(!isFalse(ary[i]))
+        res.push(ary[i])
+    }
+    return res
+  }
+
+  function drop(ary, num = 1) {
+    var len = ary.length
+    if(num >= len)
+      return[]
+    var res = []
+    for(let i = num; i < len; ++i) {
+      res.push(ary[i])
+    }
+    return res
+  }
   return {
+    drop,
+    compact,
     chunk,
   }
 }()
